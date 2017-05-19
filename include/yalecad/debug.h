fa41a7a506cac6ed3caff016e222ba7126015b03
Nils Berg <berg.nils@gmail.com>
2017-05-19 18:18:14 +0200

    whitespace ydebug.c debug.h

--- include/yalecad/debug.h
+++ include/yalecad/debug.h
@@ -1,22 +1,22 @@
-/* ----------------------------------------------------------------- 
-FILE:	    debug.h                                       
+/* -----------------------------------------------------------------
+FILE:       debug.h
 CONTENTS:   debug macros
-DATE:	    May 04, 1988 
-	    static char SccsId[] = "@(#) debug.h version 1.14 3/23/92" ;
+DATE:       May 04, 1988
+            static char SccsId[] = "@(#) debug.h version 1.14 3/23/92" ;
 REVISIONS:  Jan 24, 1989 - added selective turnoff of print debug
-		or assertions.
-	    Jan 29, 1989 - made output nicer.
-	    Apr 10, 1990 - added new debug macros and updated for
-		the debug utilities.
-	    Apr 29, 1990 - fixed cleanup handler problem.  Also
-		modified ASSERTs to call YdebugAssert instead of
-		Ydebug so that ASSERTs are always on.
-	    Sep  9, 1990 - added DS macro - DEBUG source macro
-		that is compiled when DEBUG is true. This is good
-		for definitions etc.
-	    Thu Mar  5 03:38:04 EST 1992 - better output messages
-		at the expense of not allow multiple line test without
-		a \ continuation character.
+                or assertions.
+            Jan 29, 1989 - made output nicer.
+            Apr 10, 1990 - added new debug macros and updated for
+                the debug utilities.
+            Apr 29, 1990 - fixed cleanup handler problem.  Also
+                modified ASSERTs to call YdebugAssert instead of
+                Ydebug so that ASSERTs are always on.
+            Sep  9, 1990 - added DS macro - DEBUG source macro
+                that is compiled when DEBUG is true. This is good
+                for definitions etc.
+            Thu Mar  5 03:38:04 EST 1992 - better output messages
+                at the expense of not allow multiple line test without
+                a \ continuation character.
 ----------------------------------------------------------------- */
 #ifndef DEBUG_H
 #define DEBUG_H
@@ -42,70 +42,70 @@ REVISIONS:  Jan 24, 1989 - added selective turnoff of print debug
 /* NORMAL ASSERT - if not true, output message */
 #define ASSERT(test_xz,routine_xz,userMsg_xz) \
     {if (YdebugAssert()) {\
-	if (!(test_xz)) {\
-	    char assertMsg_xz[LRECL] ; \
-	    sprintf( assertMsg_xz,\
-		"Assertion failed in file %s, line %d:\n\t",\
-		__FILE__, __LINE__ ) ;\
-	    M(ERRMSG,routine_xz,assertMsg_xz ) ;\
-	    sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
-	    M(MSG,NULL,assertMsg_xz) ;\
+        if (!(test_xz)) {\
+            char assertMsg_xz[LRECL] ; \
+            sprintf( assertMsg_xz,\
+                "Assertion failed in file %s, line %d:\n\t",\
+                __FILE__, __LINE__ ) ;\
+            M(ERRMSG,routine_xz,assertMsg_xz ) ;\
+            sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
+            M(MSG,NULL,assertMsg_xz) ;\
     }}}
 
 /* ASSERT and if not true break */
 #define ASSERTNBREAK(test_xz,routine_xz,userMsg_xz) \
     {if (YdebugAssert()) {\
-	if (!(test_xz)) {\
-	    char assertMsg_xz[LRECL] ; \
-	    sprintf( assertMsg_xz,\
-		"Assertion failed in file %s, line %d:\n\t",\
-		__FILE__, __LINE__ ) ;\
-	    M(ERRMSG,routine_xz,assertMsg_xz ) ;\
-	    sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
-	    M(MSG,NULL,assertMsg_xz) ;\
-	    break ; \
+        if (!(test_xz)) {\
+            char assertMsg_xz[LRECL] ; \
+            sprintf( assertMsg_xz,\
+                "Assertion failed in file %s, line %d:\n\t",\
+                __FILE__, __LINE__ ) ;\
+            M(ERRMSG,routine_xz,assertMsg_xz ) ;\
+            sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
+            M(MSG,NULL,assertMsg_xz) ;\
+            break ; \
     }}}
 
 /* ASSERT and if not true continue */
 #define ASSERTNCONT(test_xz,routine_xz,userMsg_xz) \
     {if (YdebugAssert()) {\
-	if (!(test_xz)) {\
-	    char assertMsg_xz[LRECL] ; \
-	    sprintf( assertMsg_xz,\
-		"Assertion failed in file %s, line %d:\n\t",\
-		__FILE__, __LINE__ ) ;\
-	    M(ERRMSG,routine_xz,assertMsg_xz ) ;\
-	    sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
-	    M(MSG,NULL,assertMsg_xz) ;\
-	    continue ; \
+        if (!(test_xz)) {\
+            char assertMsg_xz[LRECL] ; \
+            sprintf( assertMsg_xz,\
+                "Assertion failed in file %s, line %d:\n\t",\
+                __FILE__, __LINE__ ) ;\
+            M(ERRMSG,routine_xz,assertMsg_xz ) ;\
+            sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
+            M(MSG,NULL,assertMsg_xz) ;\
+            continue ; \
     }}}
 
 
 /* ASSERT and if not true return */
 #define ASSERTNRETURN(test_xz,routine_xz,userMsg_xz) \
     {if (YdebugAssert()) {\
-	if (!(test_xz)) {\
-	    char assertMsg_xz[LRECL] ; \
-	    sprintf( assertMsg_xz,\
-		"Assertion failed in file %s, line %d:\n\t",\
-		__FILE__, __LINE__ ) ;\
-	    M(ERRMSG,routine_xz,assertMsg_xz ) ;\
-	    sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
-	    M(MSG,NULL,assertMsg_xz) ;\
-	    return ; \
+        if (!(test_xz)) {\
+            char assertMsg_xz[LRECL] ; \
+            sprintf( assertMsg_xz,\
+                "Assertion failed in file %s, line %d:\n\t",\
+                __FILE__, __LINE__ ) ;\
+            M(ERRMSG,routine_xz,assertMsg_xz ) ;\
+            sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
+            M(MSG,NULL,assertMsg_xz) ;\
+            return ; \
     }}}
 
 #define ASSERTNRETURN2(test_xz,routine_xz,userMsg_xz) \
     {if (YdebugAssert()) {\
-	if (!(test_xz)) {\
-	    char assertMsg_xz[LRECL] ; \
-	    sprintf( assertMsg_xz,\
-		"Assertion failed in file %s, line %d:\n\t",\
-		__FILE__, __LINE__ ) ;\
-	    M(ERRMSG,routine_xz,assertMsg_xz ) ;\
-	    sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
-	    M(MSG,NULL,assertMsg_xz) ;\
-	    return(NULL) ; \
+        if (!(test_xz)) {\
+            char assertMsg_xz[LRECL] ; \
+            sprintf( assertMsg_xz,\
+                "Assertion failed in file %s, line %d:\n\t",\
+                __FILE__, __LINE__ ) ;\
+            M(ERRMSG,routine_xz,assertMsg_xz ) ;\
+            sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
+            M(MSG,NULL,assertMsg_xz) ;\
+            return(NULL) ; \
     }}}
 
 
@@ -113,45 +113,45 @@ REVISIONS:  Jan 24, 1989 - added selective turnoff of print debug
 /* ASSERT and if not true exit program thru cleanup handler */
 #define ASSERTNFAULT(test_xz,routine_xz,userMsg_xz) \
     {if (YdebugAssert()) {\
-	if (!(test_xz)) {\
-	    char assertMsg_xz[LRECL] ; \
-	    sprintf( assertMsg_xz,\
-		"Assertion failed in file %s, line %d:\n\t",\
-		__FILE__, __LINE__ ) ;\
-	    M(ERRMSG,routine_xz,assertMsg_xz ) ;\
-	    sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
-	    M(MSG,NULL,assertMsg_xz) ;\
-	    YcleanupHandler( -1, NULL ) ; \
+        if (!(test_xz)) {\
+            char assertMsg_xz[LRECL] ; \
+            sprintf( assertMsg_xz,\
+                "Assertion failed in file %s, line %d:\n\t",\
+                __FILE__, __LINE__ ) ;\
+            M(ERRMSG,routine_xz,assertMsg_xz ) ;\
+            sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
+            M(MSG,NULL,assertMsg_xz) ;\
+            YcleanupHandler( -1, NULL ) ; \
     }}}
 
 
 /* ASSERT and ask user if he wishes to continue otherwise abort */
 #define ASSERTNQUERY(test_xz,routine_xz,userMsg_xz) \
     {if (YdebugAssert()) {\
-	if (!(test_xz)) {\
-	    char assertMsg_xz[LRECL] ; \
-	    sprintf( assertMsg_xz,\
-		"Assertion failed in file %s, line %d:\n\t",\
-		__FILE__, __LINE__ ) ;\
-	    M(ERRMSG,routine_xz,assertMsg_xz ) ;\
-	    sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
-	    M(MSG,NULL,assertMsg_xz) ;\
-	    M(MSG,NULL,"Do you wish to continue?") ;\
-	    scanf( "%s", assertMsg_xz );\
-	    if( assertMsg_xz[0] != 'Y' && assertMsg_xz[0] != 'y' ){\
-		YexitPgm( PGMFAIL ) ; \
+        if (!(test_xz)) {\
+            char assertMsg_xz[LRECL] ; \
+            sprintf( assertMsg_xz,\
+                "Assertion failed in file %s, line %d:\n\t",\
+                __FILE__, __LINE__ ) ;\
+            M(ERRMSG,routine_xz,assertMsg_xz ) ;\
+            sprintf( assertMsg_xz, "%s\n", userMsg_xz ) ;\
+            M(MSG,NULL,assertMsg_xz) ;\
+            M(MSG,NULL,"Do you wish to continue?") ;\
+            scanf( "%s", assertMsg_xz );\
+            if( assertMsg_xz[0] != 'Y' && assertMsg_xz[0] != 'y' ){\
+                YexitPgm( PGMFAIL ) ; \
     }}}}
 
 /* ASSERT and if not true execute the function */
 #define ASSERTNFUNC(test_xz,routine_xz,function_xz) \
     {if (YdebugAssert()) {\
-	if (!(test_xz)) {\
-	    { function_xz \
+        if (!(test_xz)) {\
+            { function_xz \
     }}}}
 
 /* ************** function prototypes ************** */
 extern BOOL Ydebug( P1(char *routine) ) ;
-/* 
+/*
 Function:
     Return TRUE if the given routine has been turned on for debug.
     It returns FALSE otherwise.
@@ -159,19 +159,19 @@ Function:
 
 /* ASSERTIONS are always on */
 extern BOOL YdebugAssert() ;
-/* 
+/*
 Function:
     Returns TRUE if debug is on.  It returns FALSE otherwise.
 */
 
 extern VOID YdebugWrite() ;
-/* 
+/*
 Function:
     Write the debug data structure to a file.
 */
 
 extern VOID YsetDebug( P1(BOOL flag ) ) ;
-/* 
+/*
 Function:
     Turn the debug functions on or off.  It will cause the evaluation
     of the dbg file in the current working directory.
@@ -179,14 +179,14 @@ Function:
 
 #else  /* remove debug code from source */
 
-#define ASSERT(a_xz,b_xz,c_xz) 
-#define ASSERTNBREAK(a_xz,b_xz,c_xz) 
-#define ASSERTNCONT(a_xz,b_xz,c_xz) 
-#define ASSERTNRETURN(a_xz,b_xz,c_xz) 
-#define ASSERTNFAULT(a_xz,b_xz,c_xz) 
-#define ASSERTNQUERY(a_xz,b_xz,c_xz) 
-#define ASSERTNFUNC(a_xz,b_xz,c_xz) 
-#define D(x_xz,func_xz)      
+#define ASSERT(a_xz,b_xz,c_xz)
+#define ASSERTNBREAK(a_xz,b_xz,c_xz)
+#define ASSERTNCONT(a_xz,b_xz,c_xz)
+#define ASSERTNRETURN(a_xz,b_xz,c_xz)
+#define ASSERTNFAULT(a_xz,b_xz,c_xz)
+#define ASSERTNQUERY(a_xz,b_xz,c_xz)
+#define ASSERTNFUNC(a_xz,b_xz,c_xz)
+#define D(x_xz,func_xz)
 #define DS(name_xz)
 
 #endif  /* DEBUG */
@@ -194,9 +194,9 @@ Function:
 /* now selectively delete either ASSERTIONS or PRINT */
 #ifdef TURNOFFPRINTD
 
-#undef  D      
-#define D(x_xz,func_xz)      
-#undef  DS      
+#undef  D
+#define D(x_xz,func_xz)
+#undef  DS
 #define DS(name_xz)
 
 #endif  /* end TURNOFFPTRINTD */
@@ -211,13 +211,13 @@ Function:
 #undef ASSERTNQUERY
 #undef ASSERTNFUNC
 
-#define ASSERT(a_xz,b_xz,c_xz) 
-#define ASSERTNBREAK(a_xz,b_xz,c_xz) 
-#define ASSERTNCONT(a_xz,b_xz,c_xz) 
-#define ASSERTNRETURN(a_xz,b_xz,c_xz) 
-#define ASSERTNFAULT(a_xz,b_xz,c_xz) 
-#define ASSERTNQUERY(a_xz,b_xz,c_xz) 
-#define ASSERTNFUNC(a_xz,b_xz,c_xz) 
+#define ASSERT(a_xz,b_xz,c_xz)
+#define ASSERTNBREAK(a_xz,b_xz,c_xz)
+#define ASSERTNCONT(a_xz,b_xz,c_xz)
+#define ASSERTNRETURN(a_xz,b_xz,c_xz)
+#define ASSERTNFAULT(a_xz,b_xz,c_xz)
+#define ASSERTNQUERY(a_xz,b_xz,c_xz)
+#define ASSERTNFUNC(a_xz,b_xz,c_xz)
 
 #endif /* end TURNOFFASSERT */
 
--- src/Ylib/ydebug.c
+++ src/Ylib/ydebug.c
@@ -37,35 +37,35 @@
  *
  */
 
-/* ----------------------------------------------------------------- 
-FILE:	    ydebug.c                                       
-DESCRIPTION:This file turns on and off debug testing.  Used by assert 
-and D() routines to determine whether to perform debug testing.  At 
+/* -----------------------------------------------------------------
+FILE:       ydebug.c
+DESCRIPTION:This file turns on and off debug testing.  Used by assert
+and D() routines to determine whether to perform debug testing.  At
 any time, debug can be turned on or off if the debug has been compiled
 into the code.  This gives uses maximum flexibility for debug can be
 loaded into code using -DDEBUG if asserts and D() statements are used.
 They remain dormant in code except for Ydebug() call until debugFlagS
 is turned on.  For production code, -DDEBUG should not be used.
 Each routine is stored in a red-black tree during the execution.
-CONTENTS:  Ydebug() 
-	   YsetDebug( flag ) 
-DATE:	   Apr 9, 1989 
+CONTENTS:  Ydebug()
+           YsetDebug( flag )
+DATE:      Apr 9, 1989
 REVISIONS: Apr 10, 1990 - rewrote debug routines so that each individual
     debug routine has its own name and can be turned off and on without
     recompiling.  At the end of a execution, it updates the dbg file.
     Also moved name to ydebug because of a dbx bug.
-	    Apr 28, 1990 - added YdebugAssert so that assertions
-		are not added to the tree.
-	    Fri Jan 18 18:38:36 PST 1991 - fixed to run on AIX.
-	    Fri Feb 22 23:40:32 EST 1991 - changed default to suit
-		Richard.
-	    Wed Apr 17 23:33:17 EDT 1991 - renamed treeS to debug_treeS
-		so that debugging would be easier.  Stupid dbx.
-	    Mon Aug 12 15:54:00 CDT 1991 - update for new
-		Yrbtree_init.
-	    Sun Nov  3 13:13:12 EST 1991 - fixed gcc complaints.
-	    Tue Feb  4 15:31:28 EST 1992 - added return_code variable to
-		Ydebug so you can switch it in the debugger if necessary.
+            Apr 28, 1990 - added YdebugAssert so that assertions
+                are not added to the tree.
+            Fri Jan 18 18:38:36 PST 1991 - fixed to run on AIX.
+            Fri Feb 22 23:40:32 EST 1991 - changed default to suit
+                Richard.
+            Wed Apr 17 23:33:17 EDT 1991 - renamed treeS to debug_treeS
+                so that debugging would be easier.  Stupid dbx.
+            Mon Aug 12 15:54:00 CDT 1991 - update for new
+                Yrbtree_init.
+            Sun Nov  3 13:13:12 EST 1991 - fixed gcc complaints.
+            Tue Feb  4 15:31:28 EST 1992 - added return_code variable to
+                Ydebug so you can switch it in the debugger if necessary.
 ----------------------------------------------------------------- */
 #ifndef lint
 static char SccsId[] = "@(#) ydebug.c (Yale) version 3.15 2/7/92" ;
@@ -92,38 +92,38 @@ static YTREEPTR debug_treeS ;
 static INT compare_routine( P2(ROUTINEPTR key1, ROUTINEPTR key2 ) ) ;
 static ROUTINEPTR make_data_debug( P2(char *string, BOOL debugOn ) ) ;
 
-BOOL Ydebug( char *routine ) 
-{   
+BOOL Ydebug( char *routine )
+{
     ROUTINEPTR data ;                 /* the matched data record */
     ROUTINE routine_key ;             /* used to test key */
-    BOOL return_code ;		      /* return code so we can set in debugger */
+    BOOL return_code ;                /* return code so we can set in debugger */
 
     if( debugFlagS ){
-	return_code = FALSE ;
-	if( routine ){
-	    routine_key.routine = routine ;
-	    if( data = (ROUTINEPTR) 
-		Yrbtree_search( debug_treeS, (char *) &(routine_key) ) ){
-		if( data->debugOn ){
-		    return_code = TRUE ;
-		}
-	    } else { /* add it to the tree */
-		if( firstTimeS ){
-		    /* the default is for it to be off */
-		    data = make_data_debug( routine, FALSE ) ;
-		    Yrbtree_insert( debug_treeS, (char *) data ) ;
-		} else {
-		    /* when adding new routines to an existing file */
-		    /* the default is for it to be off */
-		    data = make_data_debug( routine, FALSE ) ;
-		    Yrbtree_insert( debug_treeS, (char *) data ) ;
-		}
-	    }
-	} else {
-	    fprintf( stderr, "No debug routine name specified here\n" ) ;
-	}
+        return_code = FALSE ;
+        if( routine ){
+            routine_key.routine = routine ;
+            if( data = (ROUTINEPTR)
+                Yrbtree_search( debug_treeS, (char *) &(routine_key) ) ){
+                if( data->debugOn ){
+                    return_code = TRUE ;
+                }
+            } else { /* add it to the tree */
+                if( firstTimeS ){
+                    /* the default is for it to be off */
+                    data = make_data_debug( routine, FALSE ) ;
+                    Yrbtree_insert( debug_treeS, (char *) data ) ;
+                } else {
+                    /* when adding new routines to an existing file */
+                    /* the default is for it to be off */
+                    data = make_data_debug( routine, FALSE ) ;
+                    Yrbtree_insert( debug_treeS, (char *) data ) ;
+                }
+            }
+        } else {
+            fprintf( stderr, "No debug routine name specified here\n" ) ;
+        }
     } else {
-	return_code = FALSE ;
+        return_code = FALSE ;
     }
 
     return( return_code ) ;
@@ -131,8 +131,8 @@ BOOL Ydebug( char *routine )
 } /* end Ydebug */
 
 /* ASSERTIONS are always on */
-BOOL YdebugAssert() 
-{   
+BOOL YdebugAssert()
+{
     return( debugFlagS ) ;
 } /* end YdebugAssert */
 
@@ -142,20 +142,20 @@ VOID YdebugWrite()
     FILE *fp ;                   /* write to the debug file */
 
     if( debugFlagS ){
-	
-	if( YfileExists( DBGFILE ) ){
-	    /* move to .bak to save a copy of the file */
-	    sprintf( YmsgG, "/bin/cp %s %s.bak", DBGFILE,DBGFILE ) ;
-	    system( YmsgG ) ;
-	}
-	/* now write a debug file */
-	fp = TWOPEN( DBGFILE, "w", ABORT ) ;
-	for( data = (ROUTINEPTR) Yrbtree_enumerate(debug_treeS,TRUE);data; 
-	    data = (ROUTINEPTR) Yrbtree_enumerate(debug_treeS,FALSE)){
 
-	    fprintf( fp, "%s %d\n", data->routine, data->debugOn ) ;
-	}
-	TWCLOSE( fp ) ;
+        if( YfileExists( DBGFILE ) ){
+            /* move to .bak to save a copy of the file */
+            sprintf( YmsgG, "/bin/cp %s %s.bak", DBGFILE,DBGFILE ) ;
+            system( YmsgG ) ;
+        }
+        /* now write a debug file */
+        fp = TWOPEN( DBGFILE, "w", ABORT ) ;
+        for( data = (ROUTINEPTR) Yrbtree_enumerate(debug_treeS,TRUE);data;
+            data = (ROUTINEPTR) Yrbtree_enumerate(debug_treeS,FALSE)){
+
+            fprintf( fp, "%s %d\n", data->routine, data->debugOn ) ;
+        }
+        TWCLOSE( fp ) ;
     }
 }
 
@@ -170,27 +170,27 @@ VOID YsetDebug( BOOL flag )
     ROUTINEPTR data ;       /* new data to be stored */
 
     if( flag ){
-	/* initialize tree of routine name */
-	YRBTREE_INIT( debug_treeS, compare_routine );
-	line = 0 ;
-	if( fp = TWOPEN( DBGFILE, "r", NOABORT ) ){
-	    while( bufferptr=fgets(buffer,LRECL,fp )){
-		/* parse file */
-		line ++ ; /* increment line number */
-		tokens = Ystrparser( bufferptr, " \t\n", &numtokens );
+        /* initialize tree of routine name */
+        YRBTREE_INIT( debug_treeS, compare_routine );
+        line = 0 ;
+        if( fp = TWOPEN( DBGFILE, "r", NOABORT ) ){
+            while( bufferptr=fgets(buffer,LRECL,fp )){
+                /* parse file */
+                line ++ ; /* increment line number */
+                tokens = Ystrparser( bufferptr, " \t\n", &numtokens );
 
-		if( numtokens == 2 ){
-		    data = make_data_debug( tokens[0], atoi(tokens[1]) ) ;
-		    Yrbtree_insert( debug_treeS, (char *) data ) ;
-		} else {
-		    sprintf( YmsgG, "Syntax error on line:%d\n", line ) ;
-		    M(ERRMSG, "YsetDebug", YmsgG ) ;
-		} 
-	    }
-	    TWCLOSE( fp ) ;
-	} else {
-	    firstTimeS = TRUE ;
-	}
+                if( numtokens == 2 ){
+                    data = make_data_debug( tokens[0], atoi(tokens[1]) ) ;
+                    Yrbtree_insert( debug_treeS, (char *) data ) ;
+                } else {
+                    sprintf( YmsgG, "Syntax error on line:%d\n", line ) ;
+                    M(ERRMSG, "YsetDebug", YmsgG ) ;
+                }
+            }
+            TWCLOSE( fp ) ;
+        } else {
+            firstTimeS = TRUE ;
+        }
     }
     debugFlagS = flag ;
 } /* end YsetDebug */
@@ -214,13 +214,13 @@ VOID YfixDebug( char *ptr, INT type )
 {
     switch( type ){
     case 0: /* integer */
-	printf( "%d\n", (INT) ptr ) ;
-	break ;
+        printf( "%d\n", (INT) ptr ) ;
+        break ;
     case 1: /* string */
-	printf( "%s\n", ptr ) ;
-	break ;
+        printf( "%s\n", ptr ) ;
+        break ;
     case 2: /* hexidecimal */
-	printf( "%0x\n", (INT) ptr ) ;
-	break ;
+        printf( "%0x\n", (INT) ptr ) ;
+        break ;
     }
 }
