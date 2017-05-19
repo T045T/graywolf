c26e2f929a7d33ace864891a534af08c79971d6a
Nils Berg <berg.nils@gmail.com>
2017-05-19 20:33:00 +0200

    whitespace grid.c

--- src/Ylib/grid.c
+++ src/Ylib/grid.c
@@ -37,34 +37,34 @@
  *
  */
 
-/* ----------------------------------------------------------------- 
-FILE:	    grid.c                                       
+/* -----------------------------------------------------------------
+FILE:       grid.c
 DESCRIPTION:These routines grid data to the specified grid.
 CONTENTS:   YforceGrid( x , y )
-		INT *x , *y ;
-	    Ygridx( x )
-		INT *x ;
-	    Ygridx( x )
-		INT *x ;
-	    Ygridx_down( x )
-		INT *x ;
-	    Ygridy_down( y )
-		INT *y ;
-	    Ygridx_up( x )
-		INT *x ;
-	    Ygridy_up( y )
-		INT *y ;
-	    Ygrid_setx( x, offset )
-		INT x, offset ;
-	    Ygrid_sety( y, offset )
-		INT y, offset ;
-	    Ygrid_getx( x, offset )
-		INT *x, *offset ;
-	    Ygrid_gety( y, offset )
-		INT *y, *offset ;
-DATE:	    Oct 27, 1989 
+                INT *x , *y ;
+            Ygridx( x )
+                INT *x ;
+            Ygridx( x )
+                INT *x ;
+            Ygridx_down( x )
+                INT *x ;
+            Ygridy_down( y )
+                INT *y ;
+            Ygridx_up( x )
+                INT *x ;
+            Ygridy_up( y )
+                INT *y ;
+            Ygrid_setx( x, offset )
+                INT x, offset ;
+            Ygrid_sety( y, offset )
+                INT y, offset ;
+            Ygrid_getx( x, offset )
+                INT *x, *offset ;
+            Ygrid_gety( y, offset )
+                INT *y, *offset ;
+DATE:       Oct 27, 1989
 REVISIONS:  Thu Apr 18 00:39:45 EDT 1991 - renamed functions so
-		that names were consistent.
+                that names were consistent.
 ----------------------------------------------------------------- */
 #ifndef lint
 static char SccsId[] = "@(#) grid.c version 1.4 4/18/91" ;
@@ -73,9 +73,9 @@ static char SccsId[] = "@(#) grid.c version 1.4 4/18/91" ;
 #include <yalecad/base.h>
 
 static INT offsetxS = 0 ;  /* initially set offset to zero */
-static INT offsetyS = 0 ;  
+static INT offsetyS = 0 ;
 static INT xgridS = 1 ;    /* default grid is one */
-static INT ygridS = 1 ;  
+static INT ygridS = 1 ;
 
 /* force coordinate to closest x grid position */
 VOID Ygridx( INT *x )
@@ -85,7 +85,7 @@ VOID Ygridx( INT *x )
 
     newx = ( (*x - offsetxS) / xgridS ) * xgridS + offsetxS ;
     if( ABS(newx + xgridS - *x) < ABS(newx - *x) ) {
-	newx += xgridS ;
+        newx += xgridS ;
     }
     /* now return values */
     *x = newx ;
@@ -100,7 +100,7 @@ VOID Ygridy( INT *y )
 
     newy = ( (*y - offsetyS) / ygridS ) * ygridS + offsetyS ;
     if( ABS(newy + ygridS - *y) < ABS(newy - *y) ) {
-	newy += ygridS ;
+        newy += ygridS ;
     }
     /* now return values */
     *y = newy ;
@@ -145,7 +145,7 @@ VOID Ygridx_up( INT *x )
 
     newx = ( (*x - offsetxS) / xgridS ) * xgridS + offsetxS ;
     if( ABS(newx - *x)) {
-	newx += xgridS ;
+        newx += xgridS ;
     }
     /* now return values */
     *x = newx ;
@@ -158,7 +158,7 @@ VOID Ygridy_up( INT *y )
     INT newy ;
     newy = ( (*y - offsetyS) / ygridS ) * ygridS + offsetyS ;
     if( ABS(newy - *y)) {
-	newy += ygridS ;
+        newy += ygridS ;
     }
     /* now return values */
     *y = newy ;
