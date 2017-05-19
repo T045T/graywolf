0ca06298642f4512703dfe285c60a670eb40e5b6
Nils Berg <berg.nils@gmail.com>
2017-05-19 18:39:44 +0200

    whitespace time.c

--- src/Ylib/time.c
+++ src/Ylib/time.c
@@ -37,15 +37,15 @@
  *
  */
 
-/* ----------------------------------------------------------------- 
-FILE:	    time.c                                       
+/* -----------------------------------------------------------------
+FILE:       time.c
 DESCRIPTION:This file contains a time utility routine which returns
-	    an ASCII string with time and date.
-CONTENTS:   
-	    char *YcurTime( time_in_sec ) 
-		INT *time_in_sec ;
+            an ASCII string with time and date.
+CONTENTS:
+            char *YcurTime( time_in_sec )
+                INT *time_in_sec ;
 
-DATE:	    Oct 23, 1988
+DATE:       Oct 23, 1988
 REVISIONS:  Apr 27, 1989 - changed to Y prefix and added time in seconds.
 ----------------------------------------------------------------- */
 #ifndef lint
@@ -70,13 +70,13 @@ char *YcurTime( INT *time_in_sec )
     t = time((TIME_T *) 0) ;
     /* return time in seconds since Jan 1, 1970 if requested */
     if( time_in_sec ){
-	*time_in_sec = t ;
+        *time_in_sec = t ;
     }
     /* get time and remove newline char */
     for( nptr = ptr = ctime(&t) ;*nptr;nptr++ ){
-	if( *nptr == '\n' ){
-	    *nptr = EOS ;
-	}
+        if( *nptr == '\n' ){
+            *nptr = EOS ;
+        }
     }
     return( ptr ) ;
 } /* end curTime */
